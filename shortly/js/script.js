883f09e913752f82177aaa80f6dce3feb9ccf953
Hilgard <hilgardvr@protonmail.com>
2025-10-04 07:56:53 +0200

    shortly

--- shortly/js/script.js
+++ shortly/js/script.js
@@ -1,3 +1,14 @@
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
+btn.addEventListener('click', navToggle)
+
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
+
 const input = document.getElementById("link-input")
 const linkForm = document.getElementById("link-form")
 const errMsg = document.getElementById("err-msg")
